13893cf0ec4c7c121f2eb112d97c5318bda09c39
kanya <qingxingmeixiang@163.com>
2018-12-17 08:27:10 +0800

    update rebase

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Android_Shopping_Cart
 
 远程服务端代码变更
+
+本地变更readme
--- app/src/main/java/com/way/rapid/shopping/help/HttpHelp.java
+++ app/src/main/java/com/way/rapid/shopping/help/HttpHelp.java
@@ -1,7 +1,8 @@
 package com.way.rapid.shopping.help;
 
 public class HttpHelp {
-    public static String URL = "http://119.29.60.170:8080/shopping/";
+    public static String URL = "http://192.168.22.2:8080/shopping/";
+//    public static String URL = "http://119.29.60.170:8080/shopping/";
     public static int TIMEOUT = 5000;
 
 }
